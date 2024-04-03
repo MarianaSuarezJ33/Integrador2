6ff34ed993bd2d355cf8f2612ee8bf7eff7c199c
Mariana Suárez Jaramillo <marianasuarezj33@gmail.com>
2024-04-02 19:46:11 -0500

    Front6

--- ServidorWebFront/src/app/app.routes.ts
+++ ServidorWebFront/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
+import { EvidenciasComponent } from './components/evidencias/evidencias.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NotasComponent } from './components/notas/notas.component';
+import { SesionesPasadasComponent } from './components/sesiones-pasadas/sesiones-pasadas.component';
+import { SubirArchivosComponent } from './components/subir-archivos/subir-archivos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'calendario', component: CalendarioComponent},
+  { path: 'evidencias', component: EvidenciasComponent},
+  { path: 'inicio', component: InicioComponent},
+  { path: 'notas', component: NotasComponent},
+  { path: 'SesionesPasadasComponent', component: SesionesPasadasComponent},
+  { path: 'SubirArchivosComponent', component: SubirArchivosComponent},
+
+];
--- ServidorWebFront/src/app/components/calendario/calendario.component.ts
+++ ServidorWebFront/src/app/components/calendario/calendario.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CalendarModule} from '@syncfusion/ej2-angular-calendars';
+
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
   imports: [],
-  templateUrl: './calendario.component.html',
+  templateUrl: './Calendario.Component.html',
   styleUrl: './calendario.component.css'
 })
 export class CalendarioComponent {
